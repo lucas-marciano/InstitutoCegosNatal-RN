408ad1ea4c8b62e57c3633e31ce21b8910463d89
lucas_marciano <lucas.almeida.marciano@gmail.com>
2013-12-02 15:29:50 -0300

    Commit inicial do projeto

---
+++ src/institutocegosnatal/rn/InstitutoCegosNatalRN.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package institutocegosnatal.rn;
+import view.TelaPrincipal;
+/**
+ *
+ * @author Lucas
+ */
+public class InstitutoCegosNatalRN {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaPrincipal tp = new TelaPrincipal();
+        tp.setVisible(true);
+    }
+    
+}
